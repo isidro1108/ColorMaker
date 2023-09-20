923a20ee8a45361305144b6ff62b0668f38569a4
ImDev11 <euris110811@gmail.com>
2023-09-20 16:31:00 -0400

    feat: random colors in the background

--- ColorMaker/MainPage.xaml.cs
+++ ColorMaker/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 {
     public partial class MainPage : ContentPage
     {
+        bool isRandom = false;
         public MainPage()
         {
             InitializeComponent();
@@ -9,20 +10,48 @@
 
         private void slider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            var red = sldRed.Value;
-            var green = sldGreen.Value;
-            var blue = sldBlue.Value;
+            if (!isRandom)
+            {
+                var red = sldRed.Value;
+                var green = sldGreen.Value;
+                var blue = sldBlue.Value;
 
-            Color color = Color.FromRgb(red, green, blue);
+                Color color = Color.FromRgb(red, green, blue);
 
-            SetColor(color);
+                SetColor(color);
+            }
         }
 
         private void SetColor(Color color)
         {
             btnRandom.BackgroundColor = color;
+            btnRandom.TextColor = 
+                color.ToHex() == "#FFFFFF" 
+                ? Color.FromArgb("#000000")
+                : Color.FromArgb("#FFFFFF");
             Container.BackgroundColor = color;
             lblHex.Text = color.ToHex();
+        }
+
+        private void btnRandom_Clicked(object sender, EventArgs e)
+        {
+            isRandom = true;
+            var random = new Random();
+            var randomColor1 = random.Next(0, 256);
+            var randomColor2 = random.Next(0, 256);
+            var randomColor3 = random.Next(0, 256);
+
+            var color = Color.FromRgb(
+                randomColor1,
+                randomColor2,
+                randomColor3);
+
+            SetColor(color);
+
+            sldRed.Value = color.Red;
+            sldGreen.Value = color.Green;
+            sldBlue.Value = color.Blue;
+            isRandom = false;
         }
     }
 }
